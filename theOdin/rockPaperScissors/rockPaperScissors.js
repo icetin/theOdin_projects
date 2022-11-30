652031d1ee313b8dfd0a0bb6926871544fac3984
icetin <icetin@gmail.com>
2022-11-30 16:02:34 +0300

    Create one round of play

--- theOdin/rockPaperScissors/rockPaperScissors.js
+++ theOdin/rockPaperScissors/rockPaperScissors.js
@@ -6,9 +6,20 @@ console.log(choices[randInd]);
 return choices[randInd];
 }
 
-const computerSelection = getComputerChoice();
-const playerSelection = prompt("Rock, Paper or Scissors?");
+const computerSelection = getComputerChoice().toLowerCase();
+const playerSelection = prompt("Rock, Paper or Scissors?").toLowerCase();
 
 function playRound(playerSelection, computerSelection) {
+    if (playerSelection === computerSelection) {
+        return "Both players chose the same thing: It's a draw!";
+    } else if (playerSelection === "rock" && computerSelection === "scissors") {
+        return "Human player wins! Rock beats Scissors."
+    } else if (playerSelection === "paper" && computerSelection === "rock") {
+        return "Human player wins! Paper beats Rock."
+    } else if (playerSelection === "scissors" && computerSelection === "paper") {
+        return "Human player wins! Scissors beat Paper."
+    } else return "Computer player wins!"
+};
 
-}
+console.log(playRound(playerSelection, computerSelection));
+
